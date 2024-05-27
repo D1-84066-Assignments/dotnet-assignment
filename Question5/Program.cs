8f3237244bc608c60cbe0fdf7c4d1cdc5f1dc4b9
D1_NIHAR_84066 <nrpradhan93@yahoo.com>
2024-05-27 21:55:51 +0530

    question 5 completed

---
+++ Question5/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Drawing;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Question5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            /*#region assignment code
+
+            Student[] student = null;
+            CreateArray(ref student);
+            Acceptinfo(ref student);
+
+            Console.WriteLine("Before reversing");
+            Printinfo(ref student);
+            ReverseArray(ref student);
+            Console.WriteLine("After Reversing ");
+            Printinfo(ref student); 
+            #endregion*/
+            string filepath = @"C:\Data\Something.txt";
+            FileStream fs = new FileStream(filepath,
+                                            FileMode.OpenOrCreate,
+                                            FileAccess.Write);
+
+  
+            BinaryFormatter writer = new BinaryFormatter();
+
+            Student student = new Student()
+                student.acceptDetails();
+            writer.Serialize(fs, student);
+
+            writer = null;
+            fs.Close();
+
+        }
+        private static void CreateArray(ref  Student [] students)
+        {
+            Console.WriteLine("Enter Size of an Array To be formed ");
+            int size =Convert.ToInt32(Console.ReadLine());
+            students = new Student[size];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i] = new Student();
+            }
+        }
+
+        private static void Acceptinfo(ref Student[] student ) {
+
+            for (int i = 0; i < student.Length; i++)
+            {
+
+                student[i].acceptDetails();
+            }
+        
+        }
+
+        private static void Printinfo(ref Student[] student )
+        {
+            foreach ( Student s in student )
+            {
+                s.printDetails();
+            }
+        }
+        
+        private static void ReverseArray(ref Student[] students)
+        {
+            Student[] temp = new Student[students.Length];
+             
+            int j = students.Length-1;
+            for (int i = 0;i < temp.Length;i++)
+            {
+                temp[i] = students[j--];
+            }
+            students = temp;
+        }
+
+    }
+}
---
+++ Question5/Student.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question5
+{
+    [Serializable()]
+    
+    internal struct Student
+    {
+        #region members
+        String name;
+        bool gender;
+        int age;
+        int std;
+        char div;
+        double marks; 
+        #endregion
+
+        #region Ctors
+        public Student()
+        {
+            name = "new Student";
+            gender = false;
+            age = 0;
+            std = 0;
+            div = 'z';
+            marks = 0;
+
+        }
+
+        public Student(String name, bool gender, int age, int std, char div, double marks)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.age = age;
+            this.std = std;
+            this.div = div;
+            this.marks = marks;
+
+        } 
+        #endregion
+
+        #region getters/setters
+
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public bool Gender
+        {
+            get { return gender; }
+            set { gender = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+
+        public int Std
+        {
+            get { return std; }
+            set { std = value; }
+        }
+
+        public char Div
+        {
+            get { return div; }
+            set { div = value; }
+        }
+
+        public double Marks
+        {
+            get { return marks; }
+            set { marks = value; }
+        } 
+        #endregion
+
+        #region acceptDetails
+        public void acceptDetails()
+        {
+            Console.WriteLine("Enter Your Name =");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Enter Your gender = (1=M /0=F) ");
+            this.gender = Convert.ToBoolean(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("Enter your age =");
+            this.age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your std=");
+            this.std = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your div=");
+            this.div = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter your marks =");
+            this.marks = Convert.ToDouble(Console.ReadLine());
+        } 
+        #endregion
+
+        #region printDetails
+        public void printDetails()
+        {
+            Console.WriteLine("Name :");
+            Console.WriteLine(this.name);
+            Console.WriteLine("Gender :");
+            if (this.gender == true)
+            {
+                Console.WriteLine("Male");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+            Console.WriteLine("Age :");
+            Console.WriteLine(this.age);
+            Console.WriteLine("Std :");
+            Console.WriteLine(this.age);
+            Console.WriteLine("Div :");
+            Console.WriteLine(this.div);
+            Console.WriteLine("Marks :");
+            Console.WriteLine(this.marks);
+        } 
+        #endregion
+    }
+
+}
+
