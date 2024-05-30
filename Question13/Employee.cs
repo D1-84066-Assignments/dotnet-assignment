d9932e73d79fe8df77dd072940bb7c7e8c1b5c46
D1_NIHAR_84066 <nrpradhan93@yahoo.com>
2024-05-31 00:04:48 +0530

    question 13 and 14 done

---
+++ Question13/Employee.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Question13
+
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+        public string Designation { get; private set; }
+
+        public Employee(int id, string name, double salary, string designation = "Employee")
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+            Designation = designation;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Salary: {Salary}, Designation: {Designation}";
+        }
+    }
+
+}
---
+++ Question14/Program.cs
@@ -0,0 +1,19 @@
+﻿using Question13;
+namespace Question14
+{
+    internal class Program
+    {
+
+        static int Menu()
+        {
+            Console.WriteLine("1.Add Employee to company");
+            Console.WriteLine("2.Remove Employee From Company");
+            Console.WriteLine("3.Find Employee")
+            return 0;
+        }
+        static void Main(string[] args)
+        {
+       
+        }
+    }
+}
